be96119801dc67d1acc667d09970c40fb36fb467
German M. Bravo <german.mb@deipi.com>
2011-05-31 13:39:10 -0500

    Threaded indexing (no more spinning cursors)

--- python_codeintel.py
+++ python_codeintel.py
@@ -39,6 +39,7 @@ Configuration files (`~/.codeintel/config' or `project_root/.codeintel/config').
 """
 import os, sys, stat, time
 import sublime_plugin, sublime
+import threading
 import logging
 try:
     from cStringIO import StringIO
@@ -57,8 +58,11 @@ stderr_hdlr = logging.StreamHandler(sys.stderr)
 stderr_hdlr.setFormatter(logging.Formatter("%(name)s: %(levelname)s: %(message)s"))
 codeintel_log = logging.getLogger("codeintel")
 
+#codeintel_log.setLevel(logging.DEBUG)
+#codeintel_log.addHandler(stderr_hdlr)
+
 log = logging.getLogger("SublimeCodeIntel")
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.ERROR)
 log.addHandler(stderr_hdlr)
 
 cpln_fillup_chars = {
@@ -80,21 +84,42 @@ cpln_stop_chars = {
 
 status_msg = {}
 status_lineno ={}
-def calltip(view, tip, delay=0, timeout=10000, id='CodeIntel'):
+status_lock = threading.Lock()
+def calltip(view, tip, timeout=10000, delay=0, id='CodeIntel'):
     global status_msg
-    status_msg[id] = tip
+    status_lock.acquire()
+    try:
+        now = time.time()
+        status_msg[id] = tip
+    finally:
+        status_lock.release()
     def _calltip_erase():
-        if tip == status_msg.get(id):
-            view.erase_status(id)
+        status_lock.acquire()
+        try:
+            if tip == status_msg.get(id):
+                view.erase_status(id)
+        finally:
+            status_lock.release()
     def _calltip_set():
-        status_lineno[id] = view.rowcol(view.sel()[0].end())[0]
-        if tip == status_msg.get(id):
-            if tip:
-                view.set_status(id, tip)
+        lineno = view.rowcol(view.sel()[0].end())[0]
+        status_lock.acquire()
+        try:
+            if 'warning' not in id:
+                status_lineno[id] = lineno
+            current_tip = status_msg.get(id)
+            if tip == current_tip:
+                if tip:
+                    view.set_status(id, tip)
+                else:
+                    view.erase_status(id)
+            elif current_tip:
+                view.set_status(id, current_tip)
             else:
                 view.erase_status(id)
+        finally:
+            status_lock.release()
     try:
-        if delay:
+        if delay and delay is not None:
             sublime.set_timeout(_calltip_set, delay)
         else:
             _calltip_set()
@@ -103,12 +128,13 @@ def calltip(view, tip, delay=0, timeout=10000, id='CodeIntel'):
     if tip:
         sublime.set_timeout(_calltip_erase, timeout)
 
-def logger(view, type, msg=None, delay=0, timeout=None, id='CodeIntel'):
+def logger(view, type, msg=None, timeout=None, delay=0, id='CodeIntel'):
     if msg is None:
-        msg, type = type, "Info"
+        msg, type = type, 'info'
     if timeout is None:
-        timeout = { None: 3000, "Error": 3000, "Warning": 5000, "Info": 500 }.get(type)
-    calltip(view, '%s: %s' % (type, msg), delay, timeout, id + '-' + type)
+        timeout = { None: 3000, 'error': 3000, 'warning': 5000, 'info': 10000 }.get(type)
+    calltip(view, '%s: %s' % (type.capitalize(), msg), timeout=timeout, delay=delay, id=id + '-' + type)
+    getattr(log, type, log.debug)(msg)
 
 class PythonCodeIntel(sublime_plugin.EventListener):
     def on_close(self, view):
@@ -131,28 +157,33 @@ class PythonCodeIntel(sublime_plugin.EventListener):
             content = view.substr(sublime.Region(0, view.size()))
             lang, _ = os.path.splitext(os.path.basename(view.settings().get('syntax')))
             pos = view.sel()[0].end()
-            cplns, calltips = codeintel(view, path, content, lang, pos, forms=('cplns', 'calltips'))
-            if cplns is not None:
-                # Show autocompletions:
-                self.completions = sorted(
-                    [ ('%s  (%s)' % (name, type), name) for type, name in cplns ], 
-                    cmp=lambda a, b: a[1] < b[1] if a[1].startswith('_') and b[1].startswith('_') else False if a[1].startswith('_') else True if b[1].startswith('_') else a[1] < b[1]
-                )
-                view.run_command('auto_complete')
-            elif calltips is not None:
-                # Triger a tooltip
-                calltip(view, 'Tip: ' + calltips[0])
+            def _trigger(cplns, calltips):
+                if cplns is not None:
+                    # Show autocompletions:
+                    self.completions = sorted(
+                        [ ('%s  (%s)' % (name, type), name) for type, name in cplns ], 
+                        cmp=lambda a, b: a[1] < b[1] if a[1].startswith('_') and b[1].startswith('_') else False if a[1].startswith('_') else True if b[1].startswith('_') else a[1] < b[1]
+                    )
+                    sublime.set_timeout(lambda: view.run_command('auto_complete'), 0)
+                elif calltips is not None:
+                    # Triger a tooltip
+                    calltip(view, "Tip: " + calltips[0])
+            codeintel(view, path, content, lang, pos, ('cplns', 'calltips'), _trigger)
         else:
-            def scan_callback(path):
+            def save_callback(path):
                 content = view.substr(sublime.Region(0, view.size()))
                 lang, _ = os.path.splitext(os.path.basename(view.settings().get('syntax')))
                 codeintel_scan(view, path, content, lang)
-            _ci_scan_callbacks_[path] = scan_callback
+            _ci_save_callbacks_[path] = save_callback
 
     def on_selection_modified(self, view):
         lineno = view.rowcol(view.sel()[0].end())[0]
-        for id, sln in status_lineno.items():
-            if lineno != sln:
+        status_lock.acquire()
+        try:
+            slns = [ id for id, sln in status_lineno.items() if sln != lineno ]
+        finally:
+            status_lock.release()
+        for id in slns:
                 calltip(view, "", id=id)
 
     def on_query_completions(self, view, prefix, locations):
@@ -168,16 +199,19 @@ class GotoPythonDefinition(sublime_plugin.TextCommand):
         content = view.substr(sublime.Region(0, view.size()))
         lang, _ = os.path.splitext(os.path.basename(view.settings().get('syntax')))
         pos = view.sel()[0].end()
-        defns, = codeintel(view, path, content, lang, pos, forms=('defns',))
-        if defns is not None:
-            defn = defns[0]
-            path = defn.path
-            if path:
-                if defn.line:
-                    path += ':' + str(defn.line)
-                window = sublime.active_window()
-                window.open_file(path, sublime.ENCODED_POSITION)
-                window.open_file(path, sublime.ENCODED_POSITION)
+        def _trigger(defns):
+            if defns is not None:
+                defn = defns[0]
+                path = defn.path
+                if path:
+                    if defn.line:
+                        path += ':' + str(defn.line)
+                    def __trigger():
+                        window = sublime.active_window()
+                        window.open_file(path, sublime.ENCODED_POSITION)
+                        window.open_file(path, sublime.ENCODED_POSITION)
+                    sublime.set_timeout(__trigger, 0)
+        codeintel(view, path, content, lang, pos, ('defns',), _trigger)
 
 
 _ci_mgr_ = None
@@ -186,16 +220,16 @@ _ci_db_base_dir_ = None
 _ci_db_catalog_dirs_ = []
 _ci_db_import_everything_langs = None
 _ci_extra_module_dirs_ = None
-_ci_scan_callbacks_ = {}
+_ci_save_callbacks_ = {}
 _ci_save_ = 0
 
-def codeintel_scanning(force=False):
+def codeintel_save(force=False):
     global _ci_save_
-    for path, callback in _ci_scan_callbacks_.items():
-        del _ci_scan_callbacks_[path]
+    for path, callback in _ci_save_callbacks_.items():
+        del _ci_save_callbacks_[path]
         callback(path)
     if not force:
-        sublime.set_timeout(codeintel_scanning, 3000)
+        sublime.set_timeout(codeintel_save, 3000)
     # saving and culling cached parts of the database:
     if _ci_mgr_:
         _ci_save_ -= 1
@@ -204,135 +238,139 @@ def codeintel_scanning(force=False):
             _ci_mgr_.db.cull_mem()
         if _ci_save_ <= 0:
             _ci_save_ = 3
-sublime.set_timeout(codeintel_scanning, 3000)
+sublime.set_timeout(codeintel_save, 3000)
 
 def codeintel_cleanup(path):
     global _ci_mgr_
-    if path in _ci_envs_:
-        del _ci_envs_[path]
+    if _ci_envs_.pop(path, None):
         if not _ci_envs_:
-            codeintel_scanning(True)
+            codeintel_save(True)
             _ci_mgr_.finalize()
             _ci_mgr_ = None
 
-def codeintel_scan(view, path, content, lang):
-    global _ci_mgr_
+def codeintel_scan(view, path, content, lang, callback=None):
     try:
         lang = lang or guess_lang_from_path(path)
     except CodeIntelError:
-        logger(view, "Warning", "skip `%s': couldn't determine language" % path)
-        return [None] * len(forms)
-    encoding = None
+        logger(view, 'warning', "skip `%s': couldn't determine language" % path)
+        return
+    is_scratch = view.is_scratch()
+    is_dirty = view.is_dirty()
+    def _codeintel_scan():
+        global _ci_mgr_
 
-    start = None
-
-    try:
-        env = _ci_envs_[path]
-        mgr = _ci_mgr_
-    except KeyError:
-        start = time.time()
-
-        log.info("About to update indexes. The first time this can take a while. Do not despair!")
-        if _ci_mgr_:
-            mgr = _ci_mgr_
-        else:
-            mgr = Manager(
-                extra_module_dirs = _ci_extra_module_dirs_,
-                db_base_dir = _ci_db_base_dir_,
-                db_catalog_dirs = _ci_db_catalog_dirs_,
-                db_import_everything_langs = _ci_db_import_everything_langs,
-                db_event_reporter = lambda m: logger(view, m),
-            )
-            mgr.upgrade()
-            mgr.initialize()
-            _ci_mgr_ = mgr
-        
-        # Load configuration files:
-        catalogs = []
-        for catalog in mgr.db.get_catalogs_zone().avail_catalogs():
-            if catalog['lang'] == lang:
-                catalogs.append(catalog['name'])
-        log.debug("Catalogs for '%s': %s", lang, ', '.join(catalogs))
-        config = {
-            "codeintel_selected_catalogs": catalogs,
-            "codeintel_max_recursive_dir_depth": 10,
-            "codeintel_scan_files_in_project": True,
-        }
-        _config = {}
-        tryReadCodeIntelDict(os.path.expanduser(os.path.join('~', '.codeintel', 'config')), _config)
-        project_dir = path and find_folder(path, '.codeintel')
-        if project_dir:
-            tryReadCodeIntelDict(os.path.join(project_dir, 'config'), _config)
-        else:
-            project_dir = os.path.expanduser(os.path.join('~', '.codeintel'))
-        config.update(_config.get(lang, {}))
-        for conf in [ 'pythonExtraPaths', 'rubyExtraPaths', 'perlExtraPaths', 'javascriptExtraPaths', 'phpExtraPaths' ]:
-            v = config.get(conf)
-            if v and isinstance(v, (list, tuple)):
-                config[conf] = os.pathsep.join(p if p.startswith('/') else os.path.abspath(os.path.join(project_dir, '..', p)) for p in v)
-
-        env = SimplePrefsEnvironment(**config)
-
-        _ci_envs_[path] = env
-
-        calltip(view, "")
-
-    buf = mgr.buf_from_content(content, lang, env, path or "<Unsaved>", encoding)
-    if isinstance(buf, CitadelBuffer):
-        if not path or view.is_scratch():
-            buf.scan() #FIXME: Always scanning unsaved files (since many tabs can have unsaved files, or find other path as ID)
-        else:
-            _dirty = view.is_dirty()
-            if _dirty:
-                mtime = 1
-            else:
-                mtime = os.stat(path)[stat.ST_MTIME]
-            buf.scan(mtime=mtime, skip_scan_time_check=_dirty)
-        if start:
-            log.info('Ready! Scan took %s sec.' % (time.time() - start))
-        return buf
-    return None
-
-def codeintel(view, path, content, lang, pos, forms):
-    cplns = None
-    calltips = None
-    defns = None
-
-    buf = codeintel_scan(view, path, content, lang)
-    if not buf:
-        logger(view, "Error", "`%s' (%s) is not a language that uses CIX" % (path, buf.lang))
-        return [None] * len(forms)
-
-    try:
-        trg = buf.trg_from_pos(pos)
-        defn_trg = buf.defn_trg_from_pos(pos)
-    except CodeIntelError:
-        trg = None
-        defn_trg = None
-    else:
-        eval_log_stream = StringIO()
-        hdlr = logging.StreamHandler(eval_log_stream)
-        hdlr.setFormatter(logging.Formatter("%(name)s: %(levelname)s: %(message)s"))
-        codeintel_log.addHandler(hdlr)
-        ctlr = LogEvalController(codeintel_log)
         try:
-            if 'cplns' in forms and trg and trg.form == TRG_FORM_CPLN:
-                cplns = buf.cplns_from_trg(trg, ctlr=ctlr)
-            if 'calltips' in forms and trg and trg.form == TRG_FORM_CALLTIP:
-                calltips = buf.calltips_from_trg(trg, ctlr=ctlr)
-            if 'defns' in forms and defn_trg and defn_trg.form == TRG_FORM_DEFN:
-                defns = buf.defns_from_trg(defn_trg, ctlr=ctlr)
-        finally:
-            codeintel_log.removeHandler(hdlr)
-        msg = eval_log_stream.getvalue()
-        if msg:
-            logger(view, "Error", msg)
+            env = _ci_envs_[path]
+            mgr = _ci_mgr_
+        except KeyError:
+            if _ci_mgr_:
+                mgr = _ci_mgr_
+            else:
+                mgr = Manager(
+                    extra_module_dirs = _ci_extra_module_dirs_,
+                    db_base_dir = _ci_db_base_dir_,
+                    db_catalog_dirs = _ci_db_catalog_dirs_,
+                    db_import_everything_langs = _ci_db_import_everything_langs,
+                    db_event_reporter = lambda m: logger(view, m),
+                )
+                mgr.upgrade()
+                mgr.initialize()
+                _ci_mgr_ = mgr
 
-    ret = []
-    l = locals()
-    for f in forms:
-        ret.append(l.get(f))
-    return ret
+            # Load configuration files:
+            catalogs = []
+            for catalog in mgr.db.get_catalogs_zone().avail_catalogs():
+                if catalog['lang'] == lang:
+                    catalogs.append(catalog['name'])
+            log.debug("Catalogs for '%s': %s", lang, ', '.join(catalogs) or None)
+            config = {
+                "codeintel_selected_catalogs": catalogs,
+                "codeintel_max_recursive_dir_depth": 10,
+                "codeintel_scan_files_in_project": True,
+            }
+            _config = {}
+            tryReadCodeIntelDict(os.path.expanduser(os.path.join('~', '.codeintel', 'config')), _config)
+            project_dir = path and find_folder(path, '.codeintel')
+            if project_dir:
+                tryReadCodeIntelDict(os.path.join(project_dir, 'config'), _config)
+            else:
+                project_dir = os.path.expanduser(os.path.join('~', '.codeintel'))
+            config.update(_config.get(lang, {}))
+            for conf in [ 'pythonExtraPaths', 'rubyExtraPaths', 'perlExtraPaths', 'javascriptExtraPaths', 'phpExtraPaths' ]:
+                v = config.get(conf)
+                if v and isinstance(v, (list, tuple)):
+                    config[conf] = os.pathsep.join(p if p.startswith('/') else os.path.abspath(os.path.join(project_dir, '..', p)) for p in v)
+
+            env = SimplePrefsEnvironment(**config)
+
+            _ci_envs_[path] = env
+
+            calltip(view, "")
+
+        encoding = None
+        buf = mgr.buf_from_content(content, lang, env, path or "<Unsaved>", encoding)
+        if isinstance(buf, CitadelBuffer):
+            logger(view, 'warning', "Updating indexes... The first time this can take a while. Do not despair!", timeout=20000)
+            if not path or is_scratch:
+                buf.scan() #FIXME: Always scanning unsaved files (since many tabs can have unsaved files, or find other path as ID)
+            else:
+                if is_dirty:
+                    mtime = 1
+                else:
+                    mtime = os.stat(path)[stat.ST_MTIME]
+                buf.scan(mtime=mtime, skip_scan_time_check=is_dirty)
+        if callback:
+            callback(buf)
+    threading.Thread(target=_codeintel_scan).start()
+
+
+def codeintel(view, path, content, lang, pos, forms, callback=None, timeout=4000):
+    start = time.time()
+    def _codeintel(buf):
+        cplns = None
+        calltips = None
+        defns = None
+
+        if not buf:
+            logger(view, 'error', "`%s' (%s) is not a language that uses CIX" % (path, buf.lang))
+            return [None] * len(forms)
+
+        try:
+            trg = buf.trg_from_pos(pos)
+            defn_trg = buf.defn_trg_from_pos(pos)
+        except CodeIntelError:
+            trg = None
+            defn_trg = None
+        else:
+            eval_log_stream = StringIO()
+            hdlr = logging.StreamHandler(eval_log_stream)
+            hdlr.setFormatter(logging.Formatter("%(name)s: %(levelname)s: %(message)s"))
+            codeintel_log.addHandler(hdlr)
+            ctlr = LogEvalController(codeintel_log)
+            try:
+                if 'cplns' in forms and trg and trg.form == TRG_FORM_CPLN:
+                    cplns = buf.cplns_from_trg(trg, ctlr=ctlr)
+                if 'calltips' in forms and trg and trg.form == TRG_FORM_CALLTIP:
+                    calltips = buf.calltips_from_trg(trg, ctlr=ctlr)
+                if 'defns' in forms and defn_trg and defn_trg.form == TRG_FORM_DEFN:
+                    defns = buf.defns_from_trg(defn_trg, ctlr=ctlr)
+            finally:
+                codeintel_log.removeHandler(hdlr)
+            msg = eval_log_stream.getvalue()
+            if msg:
+                logger(view, 'error', msg)
+
+        ret = []
+        l = locals()
+        for f in forms:
+            ret.append(l.get(f))
+        total = (time.time() - start)
+        if total * 1000 < timeout:
+            logger(view, 'warning', "")
+            callback(*ret)
+        else:
+            logger(view, 'warning', "Just finished indexing! Please try again. Scan took %s" % total, timeout=3000)
+    codeintel_scan(view, path, content, lang, _codeintel)
 
 def find_folder(start_at, look_for):
     start_at = os.path.abspath(start_at)
