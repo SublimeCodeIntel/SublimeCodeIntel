5b5919ad960dbd69b5a553bae4a2f919320a621d
German M. Bravo <german.mb@deipi.com>
2011-06-02 02:49:10 -0500

    Removed DEBUG mode in logs

--- python_codeintel.py
+++ python_codeintel.py
@@ -93,7 +93,7 @@ codeintel_log = logging.getLogger("codeintel")
 log = logging.getLogger("SublimeCodeIntel")
 codeintel_log.handlers = [ codeintel_hdlr ]
 log.handlers = [ stderr_hdlr ]
-codeintel_log.setLevel(logging.INFO)
+codeintel_log.setLevel(logging.ERROR) # or DEBUG
 logging.getLogger("codeintel.db").setLevel(logging.INFO)
 log.setLevel(logging.ERROR)
 
