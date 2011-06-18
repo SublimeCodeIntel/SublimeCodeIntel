f00514c30a5a70aba21e05c3de9947cac39eccb6
German M. Bravo <german.mb@deipi.com>
2011-06-18 00:22:21 -0500

    Added linux binaries (thanks to Jon Skinner)

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
-# In Linux, Sublime Text's Python is compiled with UCS4, add:
-# CFLAGS=-DPy_UNICODE_SIZE 4
+#!/bin/bash
+# In Linux, Sublime Text's Python is compiled with UCS4:
+if [ $OSTYPE = "linux-gnu" ]; then
+	export CFLAGS=-DPy_UNICODE_SIZE=4
+fi
+
 unzip sgmlop-1.1.1-20040207.zip && \
 cd sgmlop-1.1.1-20040207 && \
 cat ../sgmlop*.patch | patch -sup1 && \
@@ -25,4 +28,5 @@ find . -type f -name _SilverCity.so -exec cp {} ../libs/_local_arch \; && \
 find . -type f -name ciElementTree.so -exec cp {} ../libs/_local_arch \; &&\
 rm -rf cElementTree-1.0.5-20051216 &&\
 rm -rf SilverCity-0.9.7 &&\
-rm -rf scintilla
+rm -rf scintilla &&\
+rm -rf sgmlop-1.1.1-20040207
