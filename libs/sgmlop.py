e95527390b8f44333b110e4d549ae8942cc5c801
German M. Bravo <german.mb@deipi.com>
2011-08-02 20:19:53 -0500

    Binary loaders revisited

--- libs/sgmlop.py
+++ libs/sgmlop.py
@@ -1,25 +1,25 @@
 try:
     from _local_arch.sgmlop import *
-    platform = 'Local arch'
+    platform = "Local arch"
 except ImportError:
     try:
-        from _linux_libcpp6_x86.sgmlop import *
-        platform = 'Linux 32 bits'
+        from _linux_libcpp6_x86_64.sgmlop import *
+        platform = "Linux 64 bits"
     except ImportError:
         try:
-            from _linux_libcpp6_x86_64.sgmlop import *
-            platform = 'Linux 64 bits'
+            from _linux_libcpp6_x86.sgmlop import *
+            platform = "Linux 32 bits"
         except ImportError:
             try:
-                from _win32.sgmlop import *
-                platform = 'Windows 32 bits'
+                from _win64.sgmlop import *
+                platform = "Windows 64 bits"
             except ImportError:
                 try:
-                    from _win64.sgmlop import *
-                    platform = 'Windows 64 bits'
+                    from _win32.sgmlop import *
+                    platform = "Windows 32 bits"
                 except ImportError:
                     try:
                         from _macosx_universal.sgmlop import *
-                        platform = 'MacOS X Universal'
+                        platform = "MacOS X Universal"
                     except ImportError:
-                        raise
+                        raise ImportError("Could not find a suitable sgmlop binary for your platform and architecture.")
