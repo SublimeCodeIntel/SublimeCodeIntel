5088cf6cde42c8af6f50c0fe3788851607698045
German M. Bravo <german.mb@deipi.com>
2013-07-05 16:15:24 -0500

    SilverCity lexer expects unicode

--- libs/SilverCity/Lexer.py
+++ libs/SilverCity/Lexer.py
@@ -1,7 +1,7 @@
 class Lexer:
     def tokenize_by_style(self, buffer, call_back = None):
-        if isinstance(buffer, unicode):
-            buffer = buffer.encode('utf8')
+        if isinstance(buffer, str):
+            buffer = buffer.decode('utf8')
         if call_back is not None:
             return self._lexer.tokenize_by_style(
                     buffer,
