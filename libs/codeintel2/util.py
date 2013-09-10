1e0aa3ac245f37b01b4695bdf3c0391dd1865638
German M. Bravo <german.mb@deipi.com>
2013-09-10 13:29:03 -0500

    Using Truediv for some more parts (Python 3 needs this)

--- libs/codeintel2/util.py
+++ libs/codeintel2/util.py
@@ -517,19 +517,19 @@ def banner(text, ch='=', length=78):
     """
     if text is None:
         return ch * length
-    elif len(text) + 2 + len(ch)*2 > length:
+    elif len(text) + 2 + len(ch) * 2 > length:
         # Not enough space for even one line char (plus space) around text.
         return text
     else:
         remain = length - (len(text) + 2)
-        prefix_len = remain / 2
+        prefix_len = remain // 2
         suffix_len = remain - prefix_len
         if len(ch) == 1:
             prefix = ch * prefix_len
             suffix = ch * suffix_len
         else:
-            prefix = ch * (prefix_len/len(ch)) + ch[:prefix_len % len(ch)]
-            suffix = ch * (suffix_len/len(ch)) + ch[:suffix_len % len(ch)]
+            prefix = ch * (prefix_len // len(ch)) + ch[:prefix_len % len(ch)]
+            suffix = ch * (suffix_len // len(ch)) + ch[:suffix_len % len(ch)]
         return prefix + ' ' + text + ' ' + suffix
 
 
