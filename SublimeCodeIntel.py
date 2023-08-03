bfd70f9a6c04c686c56e71b0d4431fba51f86e31
Rayyan Baig <137826567+Rayyanbaig1243@users.noreply.github.com>
2023-08-03 17:48:27 +0500

    Update SublimeCodeIntel.py

--- SublimeCodeIntel.py
+++ SublimeCodeIntel.py
@@ -1,24 +1,24 @@
-# -*- coding: utf-8 -*-
-# ***** BEGIN LICENSE BLOCK *****
-# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#-*- coding: utf-8 -*-
+#***** BEGIN LICENSE BLOCK *****
+#Version: MPL 1.1/GPL 2.0/LGPL 2.1
 #
-# The contents of this file are subject to the Mozilla Public License
-# Version 1.1 (the "License"); you may not use this file except in
-# compliance with the License. You may obtain a copy of the License at
-# http://www.mozilla.org/MPL/
+#The contents of this file are subject to the Mozilla Public License
+#Version 1.1 (the "License"); you may not use this file except in
+#compliance with the License. You may obtain a copy of the License at
+#http://www.mozilla.org/MPL/
 #
-# Software distributed under the License is distributed on an "AS IS"
-# basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-# License for the specific language governing rights and limitations
-# under the License.
+#Software distributed under the License is distributed on an "AS IS"
+#basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+#License for the specific language governing rights and limitations
+#under the License.
 #
-# The Original Code is SublimeCodeIntel code by German M. Bravo (Kronuz).
+#The Original Code is SublimeCodeIntel code by German M. Bravo (Kronuz).
 #
-# Contributor(s):
-#   ActiveState Software Inc
+#Contributor(s):
+#ActiveState Software Inc
 #
-# Portions created by ActiveState Software Inc are Copyright (C) 2000-2007
-# ActiveState Software Inc. All Rights Reserved.
+#Portions created by ActiveState Software Inc are Copyright (C) 2000-2007
+#ActiveState Software Inc. All Rights Reserved.
 #
 """
 CodeIntel is a plugin intended to display "code intelligence" information.
